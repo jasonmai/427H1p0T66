3ba896adaa1508f6b094d976b096af5d7133ee97
Alex A <Alex A>
2016-03-18 22:10:47 -0400

    Added my output

---
+++ Alex/p1_alex.R
@@ -0,0 +1,98 @@
+N <- 100
+
+indexOfFirstClosePrice <- 5
+colsPerStock <- 6
+weights <- WeightMatrix()
+
+W1 = function(t, j) {
+    if (t < 3) {
+        return(99)
+    }
+    
+    -(1/N)*(RCC(t-1, j) - AvrRCC(t-1))
+}
+
+RCC <- function(t, j) {
+    
+    if (t < 2) {
+        return(99)
+    }
+    
+    RCCResult[t,j]
+}
+
+AvrRCC <- function(t) {
+    AvrRCCResult[t]
+}
+
+RP1 <- function(t) {
+    if (t < 3) {
+        return(99)
+    }
+
+     sum(weights[t,] * RCCResult[t, ]) / sum(abs(weights[t, ]))
+}
+
+CumR <- function(t) {
+    cumRSaved <- c(99,99)
+    
+    if (t < 3) {
+        return(99)
+    }
+    
+    cumRet <- 1
+    
+    for (i in 3:t) {
+        cumRet <- cumRet * (1 + RP1(i))
+        cumRSaved <- c(cumRSaved, log(cumRet))
+    }
+    
+    cumRSaved
+}
+
+AbsWeight <- function(t) {
+    if (t < 3) {
+        return(99)
+    }
+    
+    result <- sum(abs(weights[t,]))
+
+    result
+}
+
+AbsWeightWeighted <- function(t) {
+    if (t < 3) {
+        return(99)
+    }
+    
+    sum(weights[t,]) / sum(abs(weights[t,]))
+}
+
+WeightMatrix <- function() {
+    emptyMatrix <- matrix(nrow = length(part1Data[,1]), ncol = N)
+    
+    for (t in 1: length(part1Data[,1])) {
+        for (j in 1:N) {
+            emptyMatrix[t, j] <- W1(t, j)
+        }
+    }
+    
+    emptyMatrix
+}
+
+generateReturns <- function() {
+    first5Cols <- matrix(nrow = length(part1Data[,1]), ncol = 5)
+    cumRSaved <- CumR(length(part1Data[,1]))
+    
+    for (t in 1:length(part1Data[,1])) {
+        first5Cols[t, 1] <- part1Data[t, 1]
+        first5Cols[t, 2] <- RP1(t)
+        first5Cols[t, 3] <- cumRSaved[t]
+        first5Cols[t, 4] <- AbsWeight(t)
+        first5Cols[t, 5] <- AbsWeightWeighted(t)
+    }
+    
+    result <- cbind(first5Cols, weights)
+    write.table(result, "result.txt", sep=",")
+
+}
