e381ef5824ce41086654c176acafe1710027778c
Alex A <Alex A>
2016-03-20 14:47:13 -0400

    Added part 3

---
+++ Alex/alex_general.R
@@ -0,0 +1,122 @@
+AbsWeight <- function(t, weights) {
+    if (t < 3) {
+        return(99)
+    }
+    
+    result <- sum(abs(weights[t,]))
+    
+    result
+}
+
+AbsWeightWeighted <- function(t, weights) {
+    if (t < 3) {
+        return(99)
+    }
+    
+    sum(weights[t,]) / sum(abs(weights[t,]))
+}
+
+PrintOutput <- function(allRP, weights) {
+    first5Cols <- matrix(nrow = length(part1Data[,1]), ncol = 5)
+    cumRSaved <- GetCumR(allRP)
+    allRP <- c(c(99, 99), allRP)
+    
+
+    for (t in 1:nrow(part1Data)) {
+        first5Cols[t, 1] <- part1Data[t, 1]
+        first5Cols[t, 2] <- allRP[t]
+        first5Cols[t, 3] <- cumRSaved[t]
+        print("cumr")
+        first5Cols[t, 4] <- AbsWeight(t, weights)
+        first5Cols[t, 5] <- AbsWeightWeighted(t, weights)
+        print(t)
+    }
+    
+    weights <- rbind(rep(99, ncol(weights)), weights)
+    weights <- rbind(rep(99, ncol(weights)), weights)
+    
+    result <- cbind(first5Cols, weights)
+    write.table(result, "result_part_2.txt", sep=",")
+}
+
+GetCumR <- function(allRP) {
+    cumRet <- 1
+    cumRSaved <- c(99, 99)
+    
+    for (i in 1:(nrow(part1Data) - 2)) {
+        cumRet <- cumRet * (1 + allRP[i])
+        cumRSaved <- c(cumRSaved, log(cumRet))
+    }
+    
+    cumRSaved
+}
+
+sharpeRatio <- function(allRP2) {
+    #     allRP2 <- c()
+    #     
+    #     for (t in 1:nrow(part1Data)) {
+    #         allRP2 <- c(allRP2, RP2(t))
+    #     }
+    
+    avgAllRP2 <- mean(allRP2)
+    
+    avgAllRP2 / sd(allRP2)
+}
+
+sharpeSA <- function(a) {
+    allRP2 <- AllRP2(a)
+    
+    avgAllRP2 <- mean(allRP2)
+    
+    -(avgAllRP2 / sd(allRP2))
+}
+
+sharpeSATrain <- function(a) {
+    allRP2 <- AllRP2(a)
+    
+    allRP2 <- allRP2[1:600]
+    
+    avgAllRP2 <- mean(allRP2)
+    
+    print(avgAllRP2)
+    
+    -(avgAllRP2 / sd(allRP2))
+}
+
+sharpeSATest <- function(a) {
+    allRP2 <- AllRP2(a)
+    
+    allRP2 <- allRP2[601:length(allRP2)]
+    
+    avgAllRP2 <- mean(allRP2)
+    
+    print(avgAllRP2)
+    
+    -(avgAllRP2 / sd(allRP2))
+}
+
+sharpeTrainGeneral <- function(a, funWeight, funAllRP) {
+    weights <- funWeight(a)
+    
+    allRP <- funAllRP(weights)
+    
+    allRP <- allRP[1:600]
+    
+    avgAllRP <- mean(allRP)
+    
+    -(avgAllRP / sd(allRP))
+}
+
+
+
+sharpeTestGeneral <- function(a, funWeight, funAllRP) {
+    weights <- funWeight(a)
+    
+    allRP <- funAllRP(weights)
+    
+    allRP <- allRP[600:length(allRP)]
+    
+    avgAllRP <- mean(allRP)
+    
+    -(avgAllRP / sd(allRP))
+}
--- Alex/p2_alex.R
+++ Alex/p2_alex.R
@@ -1,3 +1,5 @@
+source('alex_general.R')
+
 #Indices of various columns for all stocks
 openPriceIndices <- seq(from = 2, to = ncol(part1Data), by = 6)
 intradayHighIndices <- seq(from = 3, to = ncol(part1Data), by = 6)
@@ -9,10 +11,6 @@ RCOSub1 <- part1Data[1:(length(part1Data[,1])-1), closePriceIndices]
 RCOSub2 <- part1Data[2:length(part1Data[,1]), openPriceIndices]
 
 RCOResult <- (RCOSub2 / RCOSub1) - 1
-
-# RCOMat <- as.matrix(RCOResult)
-# RCOMat <- rbind(rep(99, N), RCOMat)
-# RCOResult <- as.data.frame(RCOMat)
 
 AvrRCOResult <- rowMeans(RCOResult)
 
@@ -30,10 +28,6 @@ ROOSub2 <- part1Data[2:length(part1Data[,1]), openPriceIndices]
 
 ROOResult <- (ROOSub2 / ROOSub1) - 1
 
-# ROOMat <- as.matrix(ROOResult)
-# ROOMat <- rbind(rep(99, N), ROOMat)
-# ROOResult <- as.data.frame(ROOMat)
-
 AvrROOResult <- rowMeans(ROOResult)
 
 #RVP Cache
@@ -48,58 +42,10 @@ TVLResult <- part1Data[, tradingVolumeIndices]
 AvrTVLResult <- computeAvrTVL()
 
 #W2 Cache
-Weights2 <- W2(rep(1, 12))
+Weights2 <- W2(c(1,0,0,0,0,0,0,0,0,0,0, 0))
 #Weights2 <- Weights2
 
-#Q's
-# Q1 <- (RCCResult - rep(AvrRCCResult, N)) / N
-# Q1 <- Q1[1: (nrow(part1Data) - 2),]
-# 
-# Q2 <- (ROOResult - rep(AvrROOResult, N)) / N
-# Q2 <- Q2[2: (nrow(part1Data) - 1),]
-# 
-# Q3 <- (ROCResult - rep(AvrROCResult, N)) / N
-# Q3 <- Q3[1: (nrow(part1Data) - 2),]
-# 
-# Q4 <- (RCOResult - rep(AvrRCOResult, N)) / N
-# Q4 <- Q4[2: (nrow(part1Data) - 1),]
-# 
-# Q5 <- (TVLResult[2: (nrow(part1Data) - 1),] / AvrTVLResult[2: (nrow(part1Data) - 1),]) * Q1
-# Q6 <- (TVLResult[2: (nrow(part1Data) - 1),] / AvrTVLResult[2: (nrow(part1Data) - 1),]) * Q2
-# Q7 <- (TVLResult[2: (nrow(part1Data) - 1),] / AvrTVLResult[2: (nrow(part1Data) - 1),]) * Q3
-# Q8 <- (TVLResult[2: (nrow(part1Data) - 1),] / AvrTVLResult[2: (nrow(part1Data) - 1),]) * Q4
-# 
-# Q9 <- (RVPResult[2: (nrow(part1Data) - 1),] / AvrRVPResult[2: (nrow(part1Data) - 1),]) * Q1
-# Q10 <- (RVPResult[2: (nrow(part1Data) - 1),] / AvrRVPResult[2: (nrow(part1Data) - 1),]) * Q2
-# Q11 <- (RVPResult[2: (nrow(part1Data) - 1),] / AvrRVPResult[2: (nrow(part1Data) - 1),]) * Q3
-# Q12 <- (RVPResult[2: (nrow(part1Data) - 1),] / AvrRVPResult[2: (nrow(part1Data) - 1),]) * Q4
-# 
-
 W2 <- function(a) {
-#     if (t < 3) {
-#         return(99)
-#     }
-#     
-#     if (length(a) != 12) {
-#         print("a needs to be a vector of length 12!")
-#         return(99)
-#     }
-#     
-#     sum(a * c(RCCResult[t - 1, j] - AvrRCCResult[t - 1], 
-#           ROOResult[t, j] - AvrROOResult[t],
-#           ROCResult[t - 1, j] - AvrROCResult[t - 1],
-#           RCOResult[t, j] - AvrRCOResult[t],
-#           (TVLResult[t - 1, j] / AvrTVLResult[t - 1]) * (RCCResult[t - 1, j] - AvrRCCResult[t - 1]),
-#           (TVLResult[t - 1, j] / AvrTVLResult[t - 1]) * (ROOResult[t, j] - AvrROOResult[t]),
-#           (TVLResult[t - 1, j] / AvrTVLResult[t - 1]) * (ROCResult[t - 1, j] - AvrROCResult[t - 1]),
-#           (TVLResult[t - 1, j] / AvrTVLResult[t - 1]) * (RCOResult[t, j] - AvrRCOResult[t]),
-#           (RVPResult[t - 1, j] / AvrRVPResult[t - 1, j]) * (RCCResult[t - 1, j] - AvrRCCResult[t - 1]),
-#           (RVPResult[t - 1, j] / AvrRVPResult[t - 1, j]) * (ROOResult[t, j] - AvrROOResult[t]),
-#           (RVPResult[t - 1, j] / AvrRVPResult[t - 1, j]) * (ROCResult[t - 1, j] - AvrROCResult[t - 1]),
-#           (RVPResult[t - 1, j] / AvrRVPResult[t - 1, j]) * (RCOResult[t, j] - AvrRCOResult[t])
-#           )
-#     )
-#     
     Q1 <- (RCCResult - rep(AvrRCCResult, N)) / N
     Q1 <- Q1[1: (nrow(part1Data) - 2),]
     
@@ -131,7 +77,9 @@ RP2 <- function(t) {
     sum(Weights2[t, ] * ROCResult[t + 2, ]) / sum(abs(Weights2[t, ]))
 }
 
-AllRP2 <- function() {
+AllRP2 <- function(a) {
+    Weights2 <- W2(a)
+    #taking ROC from the third day since our weights are only available from then on
     top <- as.matrix(Weights2 * ROCResult[3:nrow(ROCResult),])
     bottom <- as.matrix(abs(Weights2))
     
@@ -170,32 +118,8 @@ computeAvrRVP <- function() {
     result
 }
 
-computeAllWeights <- function(a) {
-    emptyMatrix <- matrix(nrow = length(part1Data[,1]), ncol = N)
-    
-    for (t in 1: length(part1Data[,1])) {
-        print(t)
-        for (j in 1:N) {
-            emptyMatrix[t, j] <- W2(a, t, j)
-        }
-    }
-    
-    emptyMatrix
-}
-
-sharpeRatio <- function(allRP2) {
-#     allRP2 <- c()
-#     
-#     for (t in 1:nrow(part1Data)) {
-#         allRP2 <- c(allRP2, RP2(t))
-#     }
-    
-    avgAllRP2 <- mean(allRP2)
-    
-    avgAllRP2 / sd(allRP2)
-}
-
 printWeights <- function(weights) {
     write.table(weights, "weights.txt", sep=",")
 }
 
+#PrintOutput(allRP2, Weights2)
